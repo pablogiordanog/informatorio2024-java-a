3125d3f0efbd71ed4b94d1f9af8b9c41fba49744
Pablo Giordano <pablogiordanog@gmail.com>
2024-07-19 08:19:34 -0300

    Fin de Ejercicio 1-Triangulo

---
+++ src/estructurada/practivo02/iniciado/TrianguloAsterisco.java
@@ -0,0 +1,20 @@
+package estructurada.practivo02.iniciado;
+
+/**
+ * 1. Triangulo de asteriscos : Se le solicita que creen un programa que imprima
+ * un triángulo de asteriscos en la consola. Usar bucles.
+ */
+
+public class TrianguloAsterisco {
+    public static void main(String[] args) {
+        int numFilas = 10;
+        for (int i = 1; i <= numFilas; i++) {
+            // Bucle para imprimir asteriscos
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Salto de línea después de cada fila
+            System.out.println();
+        }
+    }
+}
