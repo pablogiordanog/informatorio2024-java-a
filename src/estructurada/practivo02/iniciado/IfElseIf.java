970e6e080b1ac5de0976d622f850dfdfb25accdd
Pablo Giordano <pablogiordanog@gmail.com>
2024-07-19 08:36:42 -0300

    Fin de Ejercicio 4-IfElseIf

---
+++ src/estructurada/practivo02/iniciado/IfElseIf.java
@@ -0,0 +1,23 @@
+package estructurada.practivo02.iniciado;
+
+/**
+ * 4. De que se trata esto? : Realizar un programa que guarde
+ * un número en memoria y determine si es positivo, negativo o cero.
+ */
+
+public class IfElseIf {
+    public static void main(String[] args) {
+        //double numero = 23.4;
+        //double numero = 0.0;
+        double numero = -0.1;
+        if(numero>0.0d){
+            System.out.println("El numero " + numero + " es positivo.");
+        }else{
+            if(numero==0.0d){
+                System.out.println("El numero " + numero + " es cero.");
+            }else{
+                System.out.println("El numero " + numero + " es negativo.");
+            }
+        }
+    }
+}
