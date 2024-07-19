803f44cc4b1487d52400a01ceb5d2234ce72934a
Pablo Giordano <pablogiordanog@gmail.com>
2024-07-19 08:31:29 -0300

    Fin de Ejercicio 3-Numero par o impar

---
+++ src/estructurada/practivo02/iniciado/NumeroParOImpar.java
@@ -0,0 +1,18 @@
+package estructurada.practivo02.iniciado;
+
+/**
+ * 3.Suma de números : Realizar un programa que guarde un número en memoria y determine si es par o impar.
+ */
+
+public class NumeroParOImpar {
+    public static void main(String[] args) {
+        //double numero = 18.0;
+        double numero = 18.1;
+        //Si el residulo de la division es 0 entonces es par
+        if(numero % 2 == 0){
+            System.out.println("Numero " + numero + " es par.");
+        }else{
+            System.out.println("Numero " + numero + " es impar.");
+        }
+    }
+}
