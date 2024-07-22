c10a389cf6329ab480b66ba24fe6194ad3b4b345
Pablo Giordano <pablogiordanog@gmail.com>
2024-07-22 20:16:13 -0300

    Inversión de arreglo

---
+++ src/estructurada/practico02/caballero/InversionArreglo.java
@@ -0,0 +1,16 @@
+package estructurada.practico02.caballero;
+
+/**
+ * 5. Inversión de un arreglo:
+ * Dado un arreglo de enteros inviertalo,
+ * de tal manera que el primer elemento ahora sea el último y así sucesivamente.
+ */
+
+public class InversionArreglo {
+    public static void main(String[] args) {
+        int[] numeros = {1,2,3,4,5,6,7,8,9,10};
+        for(int i=numeros.length - 1;i>=0;i--){
+            System.out.println("Valor " + numeros[i]);
+        }
+    }
+}
